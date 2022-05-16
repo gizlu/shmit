e8edd85eec3181d65621d989047bd3203e17fb8c
gizlu <gizlu@gizlu.pl>
2022-05-16 17:09:15 +0200

    Migrate from greatest.h to (my own) shitest.h lib
    Rationale:
    - I simply don't like how libs such as greatest or gtest impose a lot of
      unnecesary and complex magic, that just goes into your way
    - I think that shitest output is way more readable

---
+++ shlag/shitest.h
@@ -0,0 +1,110 @@
+// shitest - poor's man test lib. I wrote it due to my discontent with fullbown
+// frameworks, such as gtest, which add lot of unnecesary abstraction that just
+// goes in your way (for instance - you can't just run tests in loop, good boys
+// use "parametrized test" magic). Shitest is just bunch of assert macros, wrappers
+// around printf, and short summary func. Of course it lacks some fancier, yet
+// sometimes useful features like timing, test order randomization or forking each
+// test into separate process (so other tests won't be affected by SEGFAULT). 
+// Timing or forking could be easily hacked on top of this lib, but order random-
+// ization obviously won't work with shitest aproach (at least not that smoothly
+// like with other libs)
+//
+// In *one* of C or C++ file, you have to define SHITEST_IMPL before including
+// shitest. For examples see `shlag/tests/` directory
+
+#ifndef SHITEST_H
+#define SHITEST_H
+#include <stdio.h>
+#include <stdbool.h>
+
+// Prepend public function definitions with whatever you want. You can use it
+// for example to make functions static. By default it does nothing
+#ifndef SHITEST_DEF
+ #define SHITEST_DEF
+#endif
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+extern unsigned testCount;
+extern unsigned passCount;
+
+// summarize tests. Returns count of failed tests
+SHITEST_DEF unsigned shi_test_summary();
+
+// Separator, you can for example print it beetween groups of tests
+#define SHI_SEP "-----\n"
+
+// Use ANSI escapes for colored output if possible. CMD doesn't support it,
+// so we disable colors on windows by default
+#if (defined(_WIN32) && !defined(SHITEST_FORCE_COLORS)) || defined(SHITEST_DISABLE_COLORS)
+ #define SHI_RED ""
+ #define SHI_GREEN ""
+ #define SHI_RESET ""
+#else 
+ #define SHI_RED "\x1b[31m"
+ #define SHI_GREEN "\x1b[32m"
+ #define SHI_RESET "\x1b[0m" // return to default color
+#endif
+
+// begin test case, with specified printf-like formatted name
+#define SHI_TESTf(fmt, ...) do { \
+ fprintf(stderr, fmt, __VA_ARGS__); \
+ fputs(": ", stderr); \
+ ++shi_testcount; \
+} while(0)
+// begin test case, with specified name
+#define SHI_TESTm(msg) SHI_TESTf("%s", msg)
+
+// mark test as passed. If you don't call it, test is considered as failed in stats
+#define SHI_PASS() do { fputs(SHI_GREEN "OK\n" SHI_RESET, stderr); ++shi_passcount; } while(0)
+
+// fail with specified formatted message (printf-like).
+// It always return false, for convieniece when writing assert macros
+SHITEST_DEF bool shi_fail_f(const char* fmt, ...);
+// just alias to shi_fail_f() func for style consistency
+#define SHI_FAILf(fmt, ...) shi_fail_f(fmt, __VA_ARGS__)
+
+// Assert that cond is true. On success return true, otherwise fail
+// with suplied msg (printf-like) and return false.
+#define SHI_ASSERTf(cond, fmt, ...) (cond) ? true : SHI_FAILf(fmt, __VA_ARGS__)
+
+// Assert that expected == actual and print their values using suplied type formatter if not
+// For example ASSERT_EQ(2, 3, "%d"); will call FAILf("expected: %d, actual: %d", 2, 3)
+#define SHI_ASSERT_EQ(expected, actual, typefmt) \
+ SHI_ASSERTf((expected) == (actual), "expected: "typefmt", actual: "typefmt, (expected), (actual))
+
+// Assert that suplied c strings are equal. On success return true, otherwise
+// fail and return false.
+#define SHI_ASSERT_STREQ(expected, actual) \
+ SHI_ASSERTf(strcmp((expected), (actual)) == 0, "expected: \"%s\", actual: \"%s\"", (expected), (actual))
+
+#ifdef __cplusplus
+ }
+#endif
+#endif /* SHLAG_PCG_H */
+
+#ifdef SHITEST_IMPL
+
+unsigned shi_testcount = 0;
+unsigned shi_passcount = 0;
+
+SHITEST_DEF bool shi_fail_f(const char* fmt, ...)
+{
+    fputs(SHI_RED"FAIL\n"SHI_RESET, stderr);
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    fputs("\n", stderr);
+    return false;
+}
+SHITEST_DEF unsigned shi_test_summary()
+{
+    unsigned failcount = shi_testcount - shi_passcount;
+    fprintf(stderr, "total: %u, passed: %u, failed: %u\n", 
+            shi_testcount, shi_passcount, failcount);
+    return (failcount > 0);
+}
+#endif // SHITEST_IMPL
--- shlag/tests/btt_test.c
+++ shlag/tests/btt_test.c
@@ -1,84 +1,89 @@
 // Compile it with something like:
-// cc -I. -Itests/vendor tests/btt_test.c -o bin/btt_test
+// cc -I. tests/btt_test.c -o bin/btt_test
 // TODO: add makefile or something
-#include "vendor/greatest.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #define SHLAG_BTT_IMPL
 #define SHLAG_BTT_DEF static // optional, but makes smaller exec
 #include "shlag_btt.h"
+#define SHITEST_IMPL
+#define SHITEST_DEF static
+#include "shitest.h"
 
-GREATEST_MAIN_DEFS();
-
-// TODO: Test big sizes.
-
-TEST b64EncodeTest(const char* in, const unsigned inSize, const char* expected, char* out)
-{
-    shlag_b64enc((unsigned char*)in, inSize, (unsigned char*)out);
-    ASSERT_STR_EQ(expected, out);
-    PASS();
-}
-
-// Test inplace and outplace encoding
-// This is the cleanest way I found, to avoid long repetitive test cases without
-// sharing buffer between unrelated tests (it could potentialy hide OOBs from sanitizer)
-#define BASE64_ENC_TEST(plain, expected) \
+// Test inplace and outplace encoding. Last byte of @in is not encoded
+// This is the cleanest (xD) way I found, to avoid long repetitive test cases without
+// sharing big buffer across unrelated tests (it could potentialy hide OOBs from sanitizer)
+#define B64_ENC_TEST(in, expected) \
 do { \
-    char out[sizeof(expected)]; \
-    greatest_set_test_suffix("NotInplace(" plain ")"); \
-    RUN_TESTp(b64EncodeTest, plain, sizeof(plain)-1, expected, out); \
-    /* reuse buffer for inplace encoding */ \
-    memcpy(out, plain, sizeof(plain)-1); \
-    greatest_set_test_suffix("Inplace(" plain ")"); \
-    RUN_TESTp(b64EncodeTest, out, sizeof(plain)-1, expected, out); \
+    char buf[sizeof(expected)]; \
+    SHI_TESTm("b64enc("#in"): "); \
+    shlag_b64enc((unsigned char*)in, sizeof(in)-1, (unsigned char*)buf); \
+    if(SHI_ASSERT_STREQ(expected, buf)) SHI_PASS(); \
+    \
+    SHI_TESTm("b64enc_inplace("#in"): "); \
+    memcpy(buf, in, sizeof(in)-1); /* reuse buff for inplace enc */ \
+    shlag_b64enc((unsigned char*)buf, sizeof(in)-1, (unsigned char*)buf); \
+    if(SHI_ASSERT_STREQ(expected, buf)) SHI_PASS(); \
 } while(0)
 
-
-SUITE(b64StrEncodingSuite) {
-    BASE64_ENC_TEST("foo", "Zm9v");
+void b64enc_testsuite()
+{
+    fprintf(stderr, "test if b64enc encodes data correctly (normally and inplace)\n");
     // RFC 4648 examples
-    BASE64_ENC_TEST("", "");
-    BASE64_ENC_TEST("f", "Zg==");
-    BASE64_ENC_TEST("fo", "Zm8=");
-    BASE64_ENC_TEST("foo", "Zm9v");
-    BASE64_ENC_TEST("foob", "Zm9vYg==");
-    BASE64_ENC_TEST("fooba", "Zm9vYmE=");
-    BASE64_ENC_TEST("foobar", "Zm9vYmFy");
+    B64_ENC_TEST("", "");
+    B64_ENC_TEST("f", "Zg==");
+    B64_ENC_TEST("fo", "Zm8=");
+    B64_ENC_TEST("foo", "Zm9v");
+    B64_ENC_TEST("foob", "Zm9vYg==");
+    B64_ENC_TEST("fooba", "Zm9vYmE=");
+    B64_ENC_TEST("foobar", "Zm9vYmFy");
     // Wikipedia examples
-    BASE64_ENC_TEST("sure.", "c3VyZS4=");
-    BASE64_ENC_TEST("sure", "c3VyZQ==");
-    BASE64_ENC_TEST("sur", "c3Vy");
-    BASE64_ENC_TEST("su", "c3U=");
-    BASE64_ENC_TEST("leasure.", "bGVhc3VyZS4=");
-    BASE64_ENC_TEST("easure.", "ZWFzdXJlLg==");
-    BASE64_ENC_TEST("asure.", "YXN1cmUu");
-    BASE64_ENC_TEST("sure.", "c3VyZS4=");
-}
-
-SUITE(b64BinEncodingSuite) {
+    B64_ENC_TEST("sure.", "c3VyZS4=");
+    B64_ENC_TEST("sure", "c3VyZQ==");
+    B64_ENC_TEST("sur", "c3Vy");
+    B64_ENC_TEST("su", "c3U=");
+    B64_ENC_TEST("leasure.", "bGVhc3VyZS4=");
+    B64_ENC_TEST("easure.", "ZWFzdXJlLg==");
+    B64_ENC_TEST("asure.", "YXN1cmUu");
+    B64_ENC_TEST("sure.", "c3VyZS4=");
     // RFC 3548 examples
-    BASE64_ENC_TEST("\x14\xfb\x9c\x03\xd9\x7e", "FPucA9l+");
-    BASE64_ENC_TEST("\x14\xfb\x9c\x03\xd9", "FPucA9k=");
-    BASE64_ENC_TEST("\x14\xfb\x9c\x03", "FPucAw==");
+    B64_ENC_TEST("\x14\xfb\x9c\x03\xd9\x7e", "FPucA9l+");
+    B64_ENC_TEST("\x14\xfb\x9c\x03\xd9", "FPucA9k=");
+    B64_ENC_TEST("\x14\xfb\x9c\x03", "FPucAw==");
     // My own test cases with nulls
-    BASE64_ENC_TEST("\0", "AA==");
-    BASE64_ENC_TEST("\0a", "AGE=");
-    BASE64_ENC_TEST("a\0b\0", "YQBiAA==");
-    BASE64_ENC_TEST("\0\0\0\0", "AAAAAA==");
+    B64_ENC_TEST("\0", "AA==");
+    B64_ENC_TEST("\0a", "AGE=");
+    B64_ENC_TEST("a\0b\0", "YQBiAA==");
+    B64_ENC_TEST("\0\0\0\0", "AAAAAA==");
+    fputs(SHI_SEP, stderr);
 }
 
-TEST encBufSizeMacroShouldReportRightSize(void) {
-    unsigned sizelookup[] = {1,5,5,5,9,9,9,13,13,13,17,17,17,21};
+void b64encsize_test_smallsizes()
+{
+    unsigned long long sizelookup[] = {1,5,5,5,9,9,9,13,13,13,17,17,17,21};
     for(unsigned i = 0; i<sizeof(sizelookup)/sizeof(sizelookup[0]); ++i) {
-         ASSERT_EQ(sizelookup[i], SHLAG_B64_ENCSIZE(i));
+        SHI_TESTf("b64encsize(%u)", i);
+        unsigned long long result = SHLAG_B64_ENCSIZE(i);
+        if(SHI_ASSERT_EQ(sizelookup[i], result, "%llu")) SHI_PASS();
     }
-    PASS();
 }
-
-int main(int argc, char **argv) {
-    GREATEST_MAIN_BEGIN();      /* command-line options, initialization. */
-
-    RUN_SUITE(b64StrEncodingSuite);
-    RUN_SUITE(b64BinEncodingSuite);
-    RUN_TEST(encBufSizeMacroShouldReportRightSize);
-
-    GREATEST_MAIN_END();        /* display results */
+void b64encsize_test_maxsize()
+{
+    SHI_TESTm("b64_encsize(ENCSIZE_LIMIT): ");
+    unsigned long long result = SHLAG_B64_ENCSIZE(SHLAG_BTT_ENCSIZE_LIMIT);
+    if(SHI_ASSERT_EQ(1466015503705LLU, result,  "%llu")) SHI_PASS();
+}
+void b64encsize_testsuite()
+{
+    fprintf(stderr, "test if b64encsize() reports correct sizes\n");
+    b64encsize_test_smallsizes();
+    b64encsize_test_maxsize();
+    fputs(SHI_SEP, stderr);
+}
+int main()
+{
+    b64enc_testsuite();
+    b64encsize_testsuite();
+    return shi_test_summary();
 }
