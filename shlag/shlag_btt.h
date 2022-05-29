89fd689b9a0fad2d49db28639fd63a6e437f0049
gizlu <gizlu@gizlu.pl>
2022-05-29 21:54:46 +0200

    Refactor shlag_btt.h
    s/shlag_b64lookup/shlag_b64enc_lookup/g
    Rename encoder-specific lookup table so it is apparent, that it is not
    used by decoder

--- shlag/shlag_btt.h
+++ shlag/shlag_btt.h
@@ -46,15 +46,17 @@ SHLAG_BTT_DEF void shlag_b64enc(const uint8_t* in, int64_t inSize, char* out);
 // Implementation. Mainly private things
 #ifdef SHLAG_BTT_IMPL
 
-static const char* shlag_b64lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+// lookup table for converting six-bit binary into base64 character
+static const char* shlag_b64enc_lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 // encode normal block of 3 bytes (result is 4 bytes)
 static inline void shlag_b64enc_triplet(const uint8_t* in, char* out)
 {
     // yes, it is kinda hard to read as we encode from backwards (to make inplace enc possible)
-    out[3] = shlag_b64lookup[in[2] & 0x3F]; // just 6LSB from 3rd byte
-    out[2] = shlag_b64lookup[((in[1] & 0xF) << 2) | (in[2] >> 6)]; // 4LSB from 2nd, 2MSB from 3rd
-    out[1] = shlag_b64lookup[((in[0] & 0x3) << 4) | (in[1] >> 4)]; // 2LSB from 1st, 4MSB from 2nd
-    out[0] = shlag_b64lookup[in[0] >> 2]; // just 6MSB from 1st
+    out[3] = shlag_b64enc_lookup[in[2] & 0x3F]; // just 6LSB from 3rd byte
+    out[2] = shlag_b64enc_lookup[((in[1] & 0xF) << 2) | (in[2] >> 6)]; // 4LSB from 2nd, 2MSB from 3rd
+    out[1] = shlag_b64enc_lookup[((in[0] & 0x3) << 4) | (in[1] >> 4)]; // 2LSB from 1st, 4MSB from 2nd
+    out[0] = shlag_b64enc_lookup[in[0] >> 2]; // just 6MSB from 1st
 }
 
 // encode 1 or 2 byte "leftover" block that goes after packs of threes and pad it with `=` to 4 bytes
@@ -63,14 +65,14 @@ static inline void shlag_b64enc_leftover(const uint8_t* in, char* out, uint8_t l
     // yes, it is kinda hard to read as we encode from backwards (to make inplace enc possible)
     if(leftover == 2) {
         out[3] = '='; // padding
-        out[2] = shlag_b64lookup[(in[1] & 0xF) << 2]; // 4 LSB from 2nd lefover byte
-        out[1] = shlag_b64lookup[(in[0] & 0x3) << 4 | (in[1] >> 4)]; // 2LSB from 1st, then 4MSB from 2nd
-        out[0] = shlag_b64lookup[in[0] >> 2]; // 6MSB from 1st byte
+        out[2] = shlag_b64enc_lookup[(in[1] & 0xF) << 2]; // 4 LSB from 2nd lefover byte
+        out[1] = shlag_b64enc_lookup[(in[0] & 0x3) << 4 | (in[1] >> 4)]; // 2LSB from 1st, then 4MSB from 2nd
+        out[0] = shlag_b64enc_lookup[in[0] >> 2]; // 6MSB from 1st byte
     } else { /* leftover == 1 */
         out[3] = '='; // padding
         out[2] = '=';
-        out[1] = shlag_b64lookup[(in[0] & 0x3) << 4]; // 2LSB from leftover byte
-        out[0] = shlag_b64lookup[in[0] >> 2]; // 6MSB from leftover byte
+        out[1] = shlag_b64enc_lookup[(in[0] & 0x3) << 4]; // 2LSB from leftover byte
+        out[0] = shlag_b64enc_lookup[in[0] >> 2]; // 6MSB from leftover byte
     }
 }
 
