49650b08cbfee62ff78c02cb5d4d4bbf869a1467
gizlu <gizlu@mailbox.org>
2022-11-11 18:05:13 +0100

    Reorder btt test code for readibility

--- shlag/tests/btt_test.c
+++ shlag/tests/btt_test.c
@@ -24,45 +24,6 @@ typedef struct TestPair
 #define TEST_PAIR(plain, encoded) \
 {(uint8_t*)(plain), #plain, encoded, sizeof((plain))-1, strlen(encoded)}
 
-void b64enc_test(TestPair p, bool inplace)
-{
-    // We don't use one big buffer for all tests despite we can, because it
-    // it could potentialy hide OOB bugs from sanitizer
-    char* buf = malloc(p.encodedLen + 1);
-    if(inplace) {
-        shi_test("b64enc_inplace(%s)", p.plainStringized);
-        memcpy(buf, p.plain, p.plainSize);
-        shlag_b64enc((uint8_t*)buf, p.plainSize, buf);
-    } else {
-        shi_test("b64enc(%s)", p.plainStringized);
-        shlag_b64enc(p.plain, p.plainSize, buf);
-    }
-    shi_assert_streq(p.encoded, buf);
-    free(buf);
-    shi_test_end();
-}
-
-void b64dec_test(TestPair p, bool inplace)
-{
-    uint8_t* buf;
-    int64_t outsize;
-    if(inplace) {
-        shi_test("b64dec_inplace(\"%s\")", p.encoded);
-        buf = malloc(p.encodedLen+1);
-        memcpy(buf, p.encoded, p.encodedLen+1);
-        outsize = shlag_b64dec((char*)buf, p.encodedLen, buf);
-    } else {
-        shi_test("b64dec(\"%s\")", p.encoded);
-        buf = malloc(p.plainSize);
-        outsize = shlag_b64dec(p.encoded, p.encodedLen, buf);
-    }
-    shi_assert_f(p.plainSize == outsize, 
-            "expected_size: %lld, actual_size: %lld", p.plainSize, outsize);
-    shi_assert_memeq_f(p.plain, buf, p.plainSize, "result != %s", p.plainStringized);
-    shi_test_end();
-    free(buf);
-}
-
 TestPair valid_b64_pairs[] = {
     // RFC 4648 examples
     TEST_PAIR("", ""),
@@ -92,21 +53,81 @@ TestPair valid_b64_pairs[] = {
     TEST_PAIR("\0\0\0\0", "AAAAAA==")
 };
 #define ARRSIZE(arr) sizeof(arr)/sizeof(arr[0])
-void b64dec_padded_testsuite(bool inplace)
+
+void b64enc_test(TestPair p, bool inplace)
 {
-    fprintf(stderr, "test b64dec %s, with valid, padded data\n",
-            inplace ? "into same buffer (inplace)" : "into external buffer");
-    for(unsigned i = 0; i<ARRSIZE(valid_b64_pairs); ++i) {
-        b64dec_test(valid_b64_pairs[i], inplace);
+    // We don't use one big buffer for all tests despite we can, because it
+    // it could potentialy hide OOB bugs from sanitizer
+    char* buf = malloc(p.encodedLen + 1);
+    if(inplace) {
+        shi_test("b64enc_inplace(%s)", p.plainStringized);
+        memcpy(buf, p.plain, p.plainSize);
+        shlag_b64enc((uint8_t*)buf, p.plainSize, buf);
+    } else {
+        shi_test("b64enc(%s)", p.plainStringized);
+        shlag_b64enc(p.plain, p.plainSize, buf);
     }
-    fputs(SHI_SEP, stderr);
+    shi_assert_streq(p.encoded, buf);
+    free(buf);
+    shi_test_end();
 }
+
 void b64enc_testsuite(bool inplace)
 {
     fprintf(stderr, "test b64enc %s\n",
             inplace ? "into same buffer (inplace)" : "into external buffer");
     for(unsigned i = 0; i<ARRSIZE(valid_b64_pairs); ++i) {
         b64enc_test(valid_b64_pairs[i], inplace);
+    }
+    fputs(SHI_SEP, stderr);
+}
+
+void b64encsize_test(int64_t in, int64_t expected)
+{
+    shi_test("b64encsize(%lld)", in);
+    shi_assert_eq(expected, SHLAG_B64_ENCSIZE(in), "%lld", int64_t);
+    shi_test_end();
+}
+
+void b64encsize_testsuite()
+{
+    fprintf(stderr, "test if b64encsize() returns correct sizes\n");
+    int64_t expected_lookup[] = {1,5,5,5,9,9,9,13,13,13,17,17,17,21};
+    for(unsigned i = 0; i<ARRSIZE(expected_lookup); ++i) {
+        b64encsize_test(i, expected_lookup[i]);
+    }
+    // max input not causing overfow
+    b64encsize_test(6917529027641081853, 9223372036854775805);
+    fputs(SHI_SEP, stderr);
+}
+
+void b64dec_test(TestPair p, bool inplace)
+{
+    uint8_t* buf;
+    int64_t outsize;
+    if(inplace) {
+        shi_test("b64dec_inplace(\"%s\")", p.encoded);
+        buf = malloc(p.encodedLen+1);
+        memcpy(buf, p.encoded, p.encodedLen+1);
+        outsize = shlag_b64dec((char*)buf, p.encodedLen, buf);
+    } else {
+        shi_test("b64dec(\"%s\")", p.encoded);
+        buf = malloc(p.plainSize);
+        outsize = shlag_b64dec(p.encoded, p.encodedLen, buf);
+    }
+    shi_assert_f(p.plainSize == outsize, 
+            "expected_size: %lld, actual_size: %lld", p.plainSize, outsize);
+    shi_assert_memeq_f(p.plain, buf, p.plainSize, "result != %s", p.plainStringized);
+    shi_test_end();
+    free(buf);
+}
+
+void b64dec_padded_testsuite(bool inplace)
+{
+    fprintf(stderr, "test b64dec %s, with valid, padded data\n",
+            inplace ? "into same buffer (inplace)" : "into external buffer");
+    for(unsigned i = 0; i<ARRSIZE(valid_b64_pairs); ++i) {
+        b64dec_test(valid_b64_pairs[i], inplace);
     }
     fputs(SHI_SEP, stderr);
 }
@@ -130,23 +151,6 @@ void b64dec_unpadded_testsuite(bool inplace)
         b64dec_test(p, inplace);
         free(p.encoded);
     }
-    fputs(SHI_SEP, stderr);
-}
-void b64encsize_test(int64_t in, int64_t expected)
-{
-    shi_test("b64encsize(%lld)", in);
-    shi_assert_eq(expected, SHLAG_B64_ENCSIZE(in), "%lld", int64_t);
-    shi_test_end();
-}
-void b64encsize_testsuite()
-{
-    fprintf(stderr, "test if b64encsize() returns correct sizes\n");
-    int64_t expected_lookup[] = {1,5,5,5,9,9,9,13,13,13,17,17,17,21};
-    for(unsigned i = 0; i<ARRSIZE(expected_lookup); ++i) {
-        b64encsize_test(i, expected_lookup[i]);
-    }
-    // max input not causing overfow
-    b64encsize_test(6917529027641081853, 9223372036854775805);
     fputs(SHI_SEP, stderr);
 }
 void b64decsize_test(int64_t in, int64_t expected)
@@ -208,11 +212,13 @@ int main()
     enum {OUTPLACE = 0, INPLACE = 1};
     b64enc_testsuite(OUTPLACE);
     b64enc_testsuite(INPLACE);
+    b64encsize_testsuite();
+
     b64dec_padded_testsuite(OUTPLACE);
     b64dec_padded_testsuite(INPLACE);
     b64dec_unpadded_testsuite(OUTPLACE);
     b64dec_unpadded_testsuite(INPLACE);
-    b64encsize_testsuite();
+
     b64decsize_testsuite();
     b64dec_invalid_testsuite();
     return (shi_test_summary() > 0);
